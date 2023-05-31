f28592ddabe62b8a2ca995425753c9a7f0a5e080
Friedrich von Grone <fitti.grone@westerbrak.de>
2023-05-31 10:57:44 +0200

    homepage

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :equipment do
-    resources :bookings, only:[:index,:new,:create,:show]
+    resources :bookings, only:[:new,:create,:show]
   end
+
+  resources :bookings, only: [:index]
   # Defines the root path route ("/")
   # root "articles#index"
 end
